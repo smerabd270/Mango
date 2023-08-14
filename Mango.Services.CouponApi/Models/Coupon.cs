ef760648a0cc4ab1dede67efefe1a88ec069355e
Amer <a.abdalrazak@transtek.com>
2023-08-14 11:12:59 +0300

    add packages CouponServices

---
+++ Mango.Services.CouponApi/Models/Coupon.cs
@@ -0,0 +1,11 @@
+﻿namespace Mango.Services.CouponApi.Models
+{
+    public class Coupon
+    {
+        public int CouponId { get; set; }
+        public string CouponCode { get; set; } = null!;
+        public double DissCountAmount { get; set; }
+        public int MinAmount { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
---
+++ Mango.Services.CouponApi/Models/Dto/CouponDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Mango.Services.CouponApi.Models.Dto
+{
+    public class CouponDto
+    {
+        public string CouponCode { get; set; } = null!;
+        public double DissCountAmount { get; set; }
+        public int MinAmount { get; set; }
+    }
+}
