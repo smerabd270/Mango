863230b2f6e01abd255b4ad111f106c0a746be61
Amer <a.abdalrazak@transtek.com>
2023-08-14 12:12:35 +0300

    ttt

---
+++ Mango.Services.CouponApi/Migrations/20230814090916_IntialDatabase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mango.Services.CouponApi.Migrations
+{
+    public partial class IntialDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "coupons",
+                columns: table => new
+                {
+                    CouponId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CouponCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DissCountAmount = table.Column<double>(type: "float", nullable: false),
+                    MinAmount = table.Column<int>(type: "int", nullable: false),
+                    LastUpdated = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_coupons", x => x.CouponId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "coupons");
+        }
+    }
+}
--- Mango.Services.CouponApi/Program.cs
+++ Mango.Services.CouponApi/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<ApplictionDbContext>(options => options.UseSqlServer(""));
+builder.Services.AddDbContext<ApplictionDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefualtConnection")));
 
 var app = builder.Build();
 
