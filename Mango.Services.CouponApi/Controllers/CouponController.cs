812dc78cb79b7a5ff4d3e68c8052d5142261fcc0
Amer <a.abdalrazak@transtek.com>
2023-08-14 15:07:37 +0300

    Add ResponseDto

--- Mango.Services.CouponApi/Controllers/CouponController.cs
+++ Mango.Services.CouponApi/Controllers/CouponController.cs
@@ -1,5 +1,6 @@
 ﻿using Mango.Services.CouponApi.Data;
 using Mango.Services.CouponApi.Models;
+using Mango.Services.CouponApi.Models.Dto;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,25 +11,50 @@ namespace Mango.Services.CouponApi.Controllers
     public class CouponController : ControllerBase
     {
         private readonly ApplictionDbContext _dbContext;
-      
+        private readonly ResponseDto _responseDto;
+
         public CouponController(ApplictionDbContext dbContext)
         {
-            _dbContext= dbContext;  
+            _dbContext = dbContext;
+            _responseDto = new ResponseDto();
         }
         [HttpGet]
-        public object GetAll()
+        public ResponseDto GetAll()
         {
             try
             {
-                IEnumerable<Coupon>copounList=_dbContext.coupons.ToList();  
-                return copounList;
+                IEnumerable<Coupon> copounList = _dbContext.coupons.ToList();
+                _responseDto.Result= copounList;
 
             }
-            catch (Exception ex) 
+            catch (Exception ex)
             {
 
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
+
             }
-            return null;
+            return _responseDto;
+        }
+        [HttpGet]
+        [Route("{id:int}")]
+        public ResponseDto Get(int id)
+
+        {
+            try
+            {
+                Coupon copoun = _dbContext.coupons.First(x=>x.CouponId==id);
+                _responseDto.Result= copoun;
+
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess=false; 
+                _responseDto.Message=ex.Message;
+
+            }
+            return _responseDto;
+
         }
     }
 }
