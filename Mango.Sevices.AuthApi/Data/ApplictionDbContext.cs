a7aaedbb060adc89eaa6e1a29c7c736edd8c29ea
amer <a.abdalrazak@transtek.com>
2023-08-23 02:12:45 +0300

    add Auth project

---
+++ Mango.Sevices.AuthApi/Data/ApplictionDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Sevices.AuthApi.Data
+{
+    public class ApplictionDbContext : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+        }
+
+        public ApplictionDbContext(DbContextOptions<ApplictionDbContext> options) : base(options) { }
+    }
+
+
+}
+
