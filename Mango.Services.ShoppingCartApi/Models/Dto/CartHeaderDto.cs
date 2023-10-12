7eac8a305e94b6e85bcec493b6dbabaa2b784acb
Amer <a.abdalrazak@transtek.com>
2023-10-12 14:02:11 +0300

    Add CartShopping Api

---
+++ Mango.Services.ShoppingCartApi/Models/Dto/CartHeaderDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Services.ShoppingCartApi.Models.Dto
+{
+    public class CartHeaderDto
+    {
+       
+        public int CartHeaderId { get; set; }
+        public string? UserId { get; set; }
+        public string? CouponCode { get; set; }
+        public decimal Discount { get; set; }
+        public decimal CartTotal { get; set; }
+    }
+}
