45656783bd2b32b4f0387cbd8dfe8ce889337de9
amer <a.abdalrazak@transtek.com>
2023-10-16 02:32:38 +0300

    Work in CartApiController

---
+++ Mango.Services.ShoppingCartApi/Controllers/CartApiController.cs
@@ -0,0 +1,68 @@
+﻿using AutoMapper;
+using Mango.Services.ShoppingCartApi.Data;
+using Mango.Services.ShoppingCartApi.Models;
+using Mango.Services.ShoppingCartApi.Models.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.ShoppingCartApi.Controllers
+{
+    [Route("api/cart")]
+    [ApiController]
+    public class CartApiController : ControllerBase
+    {
+
+			private readonly ApplictionDbContext _dbContext;
+			private readonly ResponseDto _responseDto;
+			private readonly IMapper _mapper;
+
+			public CartApiController(ApplictionDbContext dbContext, IMapper mapper)
+			{
+				_dbContext = dbContext;
+				_responseDto = new ResponseDto();
+				_mapper = mapper;
+			}
+		[HttpPost("CartUpsert")]
+		public async Task<ResponseDto> CartUpsert(CartDto cartDto)
+        {
+            try
+            {
+				var cartHeaderFromDb = await _dbContext.cartHeaders.FirstOrDefaultAsync(x => x.UserId == cartDto.CartHeader.UserId);
+				if(cartHeaderFromDb == null)
+                {
+					CartHeader cartHeader=_mapper.Map<CartHeader>(cartDto);	
+					_dbContext.cartHeaders.Add(cartHeader);
+				 await	_dbContext.SaveChangesAsync();
+					cartDto.CartDetails.First().CartHeaderId = cartHeader.CartHeaderId;
+					_dbContext.cartDetails.Add(_mapper.Map<CartDetails>(cartDto));
+					await _dbContext.SaveChangesAsync();
+
+
+					
+                }
+				else
+                {
+					var cartDetailsFromDb = await _dbContext.cartDetails.FirstOrDefaultAsync(
+						u => u.ProductId == cartDto.CartDetails.First().ProductId &&
+						u.CartHeaderId==cartHeaderFromDb.CartHeaderId);
+					if(cartDetailsFromDb == null)
+                    {
+
+                    }
+                    else 
+					{
+					}
+
+				}
+
+            }
+			catch (Exception ex)
+            {
+				_responseDto.Message= ex.Message.ToString();
+				_responseDto.IsSuccess=false;
+	
+            }
+        }
+		}
+}
