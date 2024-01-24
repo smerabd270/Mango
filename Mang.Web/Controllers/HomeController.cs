0834213684628677b3f9c36f12c7ae4e4835d3be
Amer <a.abdalrazak@transtek.com>
2024-01-24 14:49:05 +0300

    add cart ProductDetails post in home controller

--- Mang.Web/Controllers/HomeController.cs
+++ Mang.Web/Controllers/HomeController.cs
@@ -1,17 +1,26 @@
 ﻿using Mang.Web.Models;
+using Mang.Web.Services;
 using Mang.Web.Services.Iservices;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Diagnostics;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.IdentityModel;
+using IdentityModel;
 
 namespace Mang.Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly IProductService _productService;
-        public HomeController(IProductService productService)
+        private readonly ICartService _cartService;
+
+        public HomeController(IProductService productService, ICartService cartService)
         {
             _productService = productService;
+            _cartService = cartService;
         }
 
         public async Task<IActionResult> Index()
@@ -23,6 +32,42 @@ namespace Mang.Web.Controllers
                 list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
             }
             return View(list);
+        }
+        [Authorize]
+        [HttpPost]
+        [ActionName("ProductDetails")]
+        public async Task<IActionResult> ProductDetails(ProductDto productDto)
+        {
+            CartDto cartDto = new CartDto()
+            {
+                CartHeader = new CartHeaderDto
+                {
+                    UserId = User.Claims.Where(u => u.Type == JwtClaimTypes.Subject)?.FirstOrDefault()?.Value
+                }
+            };
+
+            CartDetailsDto cartDetails = new CartDetailsDto()
+            {
+                Count = productDto.ProductId,
+                ProductId = productDto.Count,
+            };
+
+            List<CartDetailsDto> cartDetailsDtos = new() { cartDetails };
+            cartDto.CartDetails = cartDetailsDtos;
+
+            ResponseDto? response = await _cartService.UpsertCartAsync(cartDto);
+
+            if (response != null && response.IsSuccess)
+            {
+                TempData["success"] = "Item has been added to the Shopping Cart";
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
+
+            return View(productDto);
         }
 
         public IActionResult Privacy()
--- Mang.Web/Models/ProductDto.cs
+++ Mang.Web/Models/ProductDto.cs
@@ -1,13 +1,20 @@
-﻿namespace Mang.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mang.Web.Models
 {
 	public class ProductDto
 	{
-		public int ProductId { get; set; }
-		public string Name { get; set; }
-		public double Price { get; set; }
-		public string Description { get; set; }
-		public string CategoryName { get; set; }
-		public string ImageUrl { get; set; }
-	}
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string CategoryName { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? ImageLocalPath { get; set; }
+        [Range(1, 100)]
+        public int Count { get; set; } = 1;
+        
+        public IFormFile? Image { get; set; }
+    }
 
 }
